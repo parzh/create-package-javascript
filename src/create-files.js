165df72aa7f061061776dba257ea536423bccc2e
Dima Parzhitsky <parzhitsky@gmail.com>
2020-03-09 11:40:42 +0200

    Properly return from createFile() when it ends

--- src/create-files.js
+++ src/create-files.js
@@ -13,46 +13,54 @@ async function createFiles(folder) {
 	const response = await fetch("https://github.com/parzh/package-javascript/archive/master.zip");
 	const buffer = await response.buffer();
 
-	fromBuffer(buffer, { lazyEntries: true }, async (error, zip) => {
-		if (error) throw error;
-		if (!zip) return;
+	return await new Promise((resolve, reject) => {
+		fromBuffer(buffer, { lazyEntries: true }, async (error, zip) => {
+			if (error)
+				return reject(error);
 
-		zip.on("entry", (/** @type {import("yauzl").Entry} */ entry) => {
-			if (entry.fileName.endsWith("/"))
-				return zip.readEntry();
+			if (!zip)
+				return resolve();
 
-			const entryFileNameChunks = entry.fileName.split("/");
+			const next = () => zip.entriesRead < zip.entryCount ? zip.readEntry() : resolve();
 
-			entryFileNameChunks.splice(0, 1, folder);
+			zip.on("entry", (/** @type {import("yauzl").Entry} */ entry) => {
+				if (entry.fileName.endsWith("/"))
+					return next();
 
-			const entryFileName = path.resolve(...entryFileNameChunks);
+				const entryFileNameChunks = entry.fileName.split("/");
 
-			console.log(`Creating file:\n${ entryFileName }`);
+				entryFileNameChunks.splice(0, 1, folder);
 
-			fs.mkdirSync(path.dirname(entryFileName), { recursive: true });
+				const entryFileName = path.resolve(...entryFileNameChunks);
 
-			zip.openReadStream(entry, (error, stream) => {
-				if (error) throw error;
+				console.log(`Creating file:\n${ entryFileName }`);
 
-				if (!stream)
-					return zip.readEntry();
+				fs.mkdirSync(path.dirname(entryFileName), { recursive: true });
 
-				const target = fs.createWriteStream(entryFileName);
+				zip.openReadStream(entry, (error, entryStream) => {
+					if (error)
+						return reject(error);
 
-				stream.on("end", () => {
-					console.log(); // empty line
-					target.close();
-					zip.readEntry();
+					if (!entryStream)
+						return next();
+
+					const targetFile = fs.createWriteStream(entryFileName);
+
+					entryStream.on("end", () => {
+						console.log(""); // empty line
+						targetFile.close();
+						next();
+					});
+
+					if (entryFileName.endsWith("package.json"))
+						entryStream.pipe(new RemoveAuthor()).pipe(targetFile);
+
+					else
+						entryStream.pipe(targetFile);
 				});
-
-				if (entryFileName.endsWith("package.json"))
-					stream.pipe(new RemoveAuthor()).pipe(target);
-
-				else
-					stream.pipe(target);
 			});
-		});
 
-		zip.readEntry();
+			next();
+		});
 	});
 };
